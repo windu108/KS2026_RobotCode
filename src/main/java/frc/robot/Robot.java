37767dc0b495426ac90e50e2978ac487f651d9d4
windu108 <masterwindu007@gmail.com>
2026-01-10 20:21:03 -0500

    Working arcade drive train - tested

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,40 +16,35 @@
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
+import com.ctre.phoenix.WPI_CallbackHelper;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
  * this project, you must also update the manifest file in the resource directory.
  */
 public class Robot extends TimedRobot {
-  private final VictorSPX m_leftFrontDrive = new VictorSPX(1);
-  private final VictorSPX m_leftBackDrive= new VictorSPX(2);
-  private final VictorSPX m_rightFrontDrive = new VictorSPX(3);
-  private final VictorSPX m_rightBackDrive= new VictorSPX(4);
+  private final WPI_VictorSPX m_leftFrontDrive = new WPI_VictorSPX(1);
+  private final WPI_VictorSPX m_leftBackDrive= new WPI_VictorSPX(2);
+  private final WPI_VictorSPX m_rightFrontDrive = new WPI_VictorSPX(3);
+  private final WPI_VictorSPX m_rightBackDrive= new WPI_VictorSPX(4);
 
-  // private final DifferentialDrive m_robotDrive =
-  //     new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
-  private final XboxController m_controller = new XboxController(0);
+
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftFrontDrive,m_rightFrontDrive);
+
+  private final PS4Controller m_controller = new PS4Controller(0);
   private final Timer m_timer = new Timer();
 
-  /** Called once at the beginning of the robot program. */
-  public Robot() {
-    // SendableRegistry.addChild(m_robotDrive, m_leftDrive);
-    // SendableRegistry.addChild(m_robotDrive, m_rightDrive);
-
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    //m_rightDrive.setInverted(true);
-  }
-
+  
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
@@ -70,34 +65,41 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters teleoperated mode. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() 
+  {
+
+    m_leftFrontDrive.follow(m_leftBackDrive);
+    m_rightFrontDrive.follow(m_rightBackDrive);
+    m_rightFrontDrive.setInverted(true);
+  }
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    //m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+    
+    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
   }
 
   /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {
 
-    m_leftFrontDrive.follow(m_leftBackDrive);
-    m_rightFrontDrive.follow(m_rightBackDrive);
+    // m_leftFrontDrive.follow(m_leftBackDrive);
+    // m_rightFrontDrive.follow(m_rightBackDrive);
 
-    // Just for testing
-    m_rightBackDrive.follow(m_leftBackDrive);
+    // // Just for testing
+    // m_rightBackDrive.follow(m_leftBackDrive);
 
-    m_timer.restart();
-    m_leftBackDrive.set(VictorSPXControlMode.PercentOutput, 1);
+    // m_timer.restart();
+    // m_leftBackDrive.set(VictorSPXControlMode.PercentOutput, 1);
   }
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
 
-    if (m_timer.get() > 1) {
-      m_leftBackDrive.set(VictorSPXControlMode.PercentOutput, 0);
-    }
+    // if (m_timer.get() > 1) {
+    //   m_leftBackDrive.set(VictorSPXControlMode.PercentOutput, 0);
+    // }
   }
 }
